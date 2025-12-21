07a24389f79415a3f8adcd9e25a9a671eeea6e1a
Kkickp <146531388+Kkickp@users.noreply.github.com>
2025-12-21 18:21:32 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,38 +1,7 @@
 <?php
-$mysqli = mysqli_init();
-
-$mysqli->ssl_set(
-  NULL,
-  NULL,
-  __DIR__ . "/ca.pem",
-  NULL,
-  NULL
-);
-
 var_dump(
   getenv("DB_HOST"),
   getenv("DB_USER"),
   getenv("DB_NAME")
 );
 exit;
-
-
-$mysqli->real_connect(
-  getenv("DB_HOST"),
-  getenv("DB_USER"),
-  getenv("DB_PASS"),
-  getenv("DB_NAME"),
-  4000,
-  NULL,
-  MYSQLI_CLIENT_SSL
-);
-
-if ($mysqli->connect_error) {
-  die("Koneksi gagal: " . $mysqli->connect_error);
-}
-
-$conn = $mysqli;
-?>
-
-
-
