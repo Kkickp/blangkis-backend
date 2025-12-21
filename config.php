5b09a9cce91bfebd1e73ef10a565bab105af1722
Kkickp <146531388+Kkickp@users.noreply.github.com>
2025-12-21 18:38:47 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,32 +1,7 @@
 <?php
-$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
-$user = getenv("DB_USER");   // masih aman pakai env
-$pass = getenv("DB_PASS");
-$db   = getenv("DB_NAME");
-
-$mysqli = mysqli_init();
-
-$mysqli->ssl_set(
-  NULL,
-  NULL,
-  __DIR__ . "/ca.pem",
-  NULL,
-  NULL
+var_dump(
+  getenv("DB_HOST"),
+  getenv("DB_USER"),
+  getenv("DB_NAME")
 );
-
-$mysqli->real_connect(
-  $host,
-  trim($user),
-  trim($pass),
-  trim($db),
-  4000,
-  NULL,
-  MYSQLI_CLIENT_SSL
-);
-
-if ($mysqli->connect_error) {
-  die("Koneksi gagal: " . $mysqli->connect_error);
-}
-
-$conn = $mysqli;
-echo "KONEKSI OK";
+exit;
