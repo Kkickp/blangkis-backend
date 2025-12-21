56d84a60b38650d99c14408bc921e87b3898f159
Kkickp <146531388+Kkickp@users.noreply.github.com>
2025-12-21 18:30:19 +0700

    Refactor config.php to use cleanEnv function

--- config.php
+++ config.php
@@ -1,7 +1,37 @@
 <?php
-var_dump(
-  getenv("DB_HOST"),
-  getenv("DB_USER"),
-  getenv("DB_NAME")
+function cleanEnv($key) {
+  return ltrim(trim(getenv($key)), '=');
+}
+
+$host = cleanEnv("DB_HOST");
+$user = cleanEnv("DB_USER");
+$pass = cleanEnv("DB_PASS");
+$db   = cleanEnv("DB_NAME");
+
+$mysqli = mysqli_init();
+
+$mysqli->ssl_set(
+  NULL,
+  NULL,
+  __DIR__ . "/ca.pem",
+  NULL,
+  NULL
 );
-exit;
+
+$mysqli->real_connect(
+  $host,
+  $user,
+  $pass,
+  $db,
+  4000,
+  NULL,
+  MYSQLI_CLIENT_SSL
+);
+
+if ($mysqli->connect_error) {
+  die("Koneksi gagal: " . $mysqli->connect_error);
+}
+
+$conn = $mysqli;
+?>
+
