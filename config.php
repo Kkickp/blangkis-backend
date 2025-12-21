1da4ed8bfda4e9c6046610b7ef1fcc3d53868edd
Kkickp <146531388+Kkickp@users.noreply.github.com>
2025-12-21 18:40:51 +0700

    Update config.php for secure DB connection
    Refactored database connection setup with SSL support and error handling.

--- config.php
+++ config.php
@@ -1,7 +1,37 @@
 <?php
-var_dump(
-  getenv("DB_HOST"),
-  getenv("DB_USER"),
-  getenv("DB_NAME")
+$host = trim(getenv("DB_HOST"));
+$user = trim(getenv("DB_USER"));   // HARUS root@cluster
+$pass = trim(getenv("DB_PASS"));
+$db   = trim(getenv("DB_NAME"));
+
+$mysqli = mysqli_init();
+
+if (!file_exists(__DIR__ . "/ca.pem")) {
+  die("CA PEM TIDAK DITEMUKAN");
+}
+
+$mysqli->ssl_set(
+  NULL,
+  NULL,
+  __DIR__ . "/ca.pem",
+  NULL,
+  NULL
 );
-exit;
+
+$mysqli->real_connect(
+  $host,
+  $user,
+  $pass,
+  $db,
+  4000,
+  NULL,
+  MYSQLI_CLIENT_SSL | MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT
+);
+
+if ($mysqli->connect_error) {
+  die("Koneksi gagal: " . $mysqli->connect_error);
+}
+
+$conn = $mysqli;
+echo "KONEKSI OK";
+?>
